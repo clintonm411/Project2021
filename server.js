d841b0e98a69bccd557397cb2a8eba5fcdcef4e6
Clinton M <clintonmendonca@outlook.com>
2021-10-05 00:55:33 +0400

    COnfigure post, json, cors, user & products routes

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ const server = express();
 const userRoutes = require('./routes/user-routes.js');
 // Import the user routes
 const productRoutes = require('./routes/product-routes.js');
+
+// Configuration
+//Configure for POST request
+server.use( express.urlencoded({ extended: false }) );
+//Configure for JSON request
+server.use( express.json() );
+//Configure for CORS request
+server.use( cors() );
+//Configure for routes
+server.use('/users',userRoutes)
+server.use('/products',productRoutes)
