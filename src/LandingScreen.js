e3e10c82839b0d316926bc09ad460cfcd68aca49
Clinton M <clintonmendonca@outlook.com>
2021-10-16 23:16:53 +0400

    Updated card title, description & youtubelinks

--- src/LandingScreen.js
+++ src/LandingScreen.js
@@ -15,50 +15,49 @@ function LandingScreen() {
         <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
           <div className="col" card>
             <Card
-            title="Art 1"
-            description="This is some text about the art."
+            title="Jojo's daydreams series (Letter A)"
+            description="Jojo would like to become a famous astronaut when he grows up. Learn letter A words with Jojo."
             image="./image1.jpg"
-            youtubeLink="https://www.youtube.com/embed/BK64WNo10iU?start=9"
+            youtubeLink="https://www.youtube.com/embed/poKCV9EskGg?start=9"
             ></Card>
           </div>
           <div className="col">
             <Card
-            title="Art 2"
-            description="This is some text about the art."
+            title="Jojo's daydreams series (Letter B)"
+            description="Jojo is taking a nap on the carpet, out of nowhere he spots a bee. Learn letter B word with Jojo."
             image="./image2.jpg"
-            youtubeLink=""
+            youtubeLink="https://www.youtube.com/embed/ags1UyuD5C0?start=9"
             ></Card>
           </div>
           <div className="col">
             <Card
-            title="Art 3"
-            description="This is some text about the art."
+            title="Jojo's daydreams series (Letter C)"
+            description="Care for some carrot cake? Jojo is day dreaming about cake. Let's practice the letter C sound with Jojo."
             image="./image3.jpg"
-            youtubeLink=""
+            youtubeLink="https://www.youtube.com/embed/elVNZ0nutRE?start=9"
             ></Card>
           </div>
           <div className="col" card>
             <Card
-            title="Art 1"
-            description="This is some text about the art."
+            title="Jojo's daydreams series (Letter D)"
+            description="Jojo is at the park and he wishes he had a dog. Find out what letter D words Jojo is practicing at the park."
             image="./image2.jpg"
-            youtubeLink=""
+            youtubeLink="https://www.youtube.com/embed/fcDn_7-bwdU?start=9"
             ></Card>
           </div>
           <div className="col">
             <Card
-            title="Art 2"
-            description="This is some text about the art."
+            title="Jojo's daydreams series (Letter E)"
+            description="Jojo loves to read. He is reading about elephants. Join Jojo as he discovers a few words that start with the letter E."
             image="./image3.jpg"
-            youtubeLink=""
+            youtubeLink="https://www.youtube.com/embed/3LZwKqJpypw?start=9"
             ></Card>
           </div>
           <div className="col">
             <Card
-            title="Art 3"
-            description="This is some text about the art."
-            image="./image1.jpg"
-            youtubeLink=""
+            title="Jojo's daydreams series (Letter F)"
+            description="Jojo and his family are off to the farm. Join Jojo and Hetty as they practice the letter F sound."            image="./image1.jpg"
+            youtubeLink="https://www.youtube.com/embed/thAm20mgqmY?start=9"
             ></Card>
           </div>
         </div>
