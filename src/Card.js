a7031a583dd346a51942f0c02be949249c317a33
Clinton M <clintonmendonca@outlook.com>
2021-10-17 00:31:28 +0400

    Card updated to check if youtubelink is provided

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,23 @@ function Card(props) {
     return (        
 
     <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style={{"background-image": cardUrl}}>
-        {/* <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('./image1.jpg');"> */}
           <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-            <h2 className="pt-1 mt-1 mb-1 display-6 lh-1 fw-bold">{props.description}</h2>
-            <div className="ratio ratio-1x1 mt-3 mb-3">
-                      <iframe src={props.youtubeLink} title="YouTube video" allowfullscreen></iframe>
-            </div>
+            <h2 className="pt-1 mt-1 mb-1 display-6 lh-1 fw-bold">{props.title}</h2>
+            <p className="p-1 mb-1 lh-1">{props.description}</p>
+            
+              {
+                (props.youtubeLink) &&
+                <div className="ratio ratio-1x1 mt-3 mb-3">
+                <iframe src={props.youtubeLink} title="YouTube video" allowfullscreen></iframe>
+                </div>
+              }
+              {
+                !(props.youtubeLink) &&
+                <h2>Coming Soon</h2>
+              }
+                      
+            
+            
             <ul className="d-flex list-unstyled mt-auto">
               <li className="me-auto">
                 <a href="/about"><img src="https://res.cloudinary.com/dxfkzwedw/image/upload/v1634393258/story_time_haven_unecpa.jpg" width="32" height="32" className="rounded-circle border border-white"/></a>
