a7c1d29da8ab635e874835307ec8df755fca7974
Clinton M <clintonmendonca@outlook.com>
2021-10-16 17:49:15 +0400

    LayoutRoute.js created

---
+++ src/LayoutRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route } from 'react-router-dom'; 
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+function LayoutRoute(props) {
+    return (
+        <React.Fragment>
+            <NavBar 
+                link1={{label:"Home", path: "/"}} 
+                link2={{label: "About", path: "/about"}} 
+                link3={{label: "Podcasts", path: "/podcasts"}}
+                navImage="./logo-anim-2.png"
+                signUp={{label: "Register", path: "/register"}}
+            ></NavBar>
+            <Route path={props.path} exact={props.exact} component={props.component} />
+            <Footer
+                label1 = "Home" link1="/home"
+                label2 = "About" link2="/about"
+                label3 = "Podcasts" link3="/podcasts"
+            ></Footer>
+        </React.Fragment>
+    )
+}
+
+export default LayoutRoute;
