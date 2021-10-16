08dda512d29d120db80dd79fb3e6600a0f1c05bc
Clinton M <clintonmendonca@outlook.com>
2021-10-16 17:43:35 +0400

    added heroName props to Hero.js

--- src/Hero.js
+++ src/Hero.js
@@ -2,7 +2,7 @@ function Hero(props){
     return(
     <div className="px-4 py-0.5 text-center">
         <img className="d-block mx-auto mb-4 img-fluid" src={props.heroImage} alt="Logo" width="700"/>
-        <h1 className="display-5 fw-bold">Podcast</h1>
+        <h1 className="display-5 fw-bold">{props.heroName}</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">A childrens' podcast web app which allows parents to register and access a secure area featuring an educational and entertaining ABC story series for kids ages 2-4.
                                 You're welcome to the world of fun learning adventures!</p>
