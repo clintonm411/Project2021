0404a5a41d3dafecea9453a22d3c56ec3ef77783
Clinton M <clintonmendonca@outlook.com>
2021-10-17 17:47:41 +0400

    Added Podcasts Screen

---
+++ src/PodcastsScreen.js
@@ -0,0 +1,9 @@
+import Hero from "./Hero";
+
+function AboutScreen (){
+    return(
+        <Hero heroImage="./logo-anim-2.png" heroNaheroName="About"></Hero>
+    );
+}
+
+export default AboutScreen;
