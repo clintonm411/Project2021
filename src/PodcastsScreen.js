e059c420aafc20223a8f5018b7444a872a54af83
Clinton M <clintonmendonca@outlook.com>
2021-10-20 00:42:02 +0400

    Added Listing, Podcast Screen and PodcastView

--- src/PodcastsScreen.js
+++ src/PodcastsScreen.js
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
 import Hero from "./Hero";
+import PodcastView from "./PodcastView";
 
 function ProductsScreen (){
     return(
-        <Hero heroImage="./logo-anim-2.png" heroName="Podcasts"></Hero>
+        <Fragment>
+            {/* <Hero heroImage="./logo-anim-2.png" heroName="Podcasts"></Hero> */}
+
+            <PodcastView></PodcastView>
+        </Fragment>
+        
+
+        
     );
 }
 
